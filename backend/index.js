0e18d948079db276f0cdd5f7a107a4b6821b231b
Fahim <fahim@nextsolutionlab.com>
2024-10-15 08:28:40 +0600

    deploying the site

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://steady-rolypoly-135094.netlify.app',
+    origin:'https://jobportalnsl.netlify.app',
     credentials:true
 }
 
